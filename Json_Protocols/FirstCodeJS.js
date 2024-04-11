0e8241dea8b370d1e2762fc8da745540f2c43465
OswaldMG <158839933+OswaldMG@users.noreply.github.com>
2024-04-11 07:47:36 -0600

    11/04/24

---
+++ Json_Protocols/FirstCodeJS.js
@@ -0,0 +1,10 @@
+var cadena=prompt("Escribe tu nombre","nombre");
+alert("Hola "+cadena);
+
+var num1=prompt("Escribe Un Numero","");
+var num2=prompt("Escribe Otro Numero","");
+var suma=parseInt(num1)+parseInt(num2);
+alert("La suma es "+suma);
+//parse prefijo para convertir variables a tipo de dato
+var suma=parseFloat(num1)+parseFloat(num2);
+alert("La suma es "+suma);
